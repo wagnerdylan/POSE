f3a9fa5e2115347336876ad2c554e5a17d1c9780
Dylan Wagner <vstrideultimate@gmail.com>
2024-04-29 13:40:19 -0700

    feat: add in plotting support for multiple objects

--- util/graphical_output_inspector.py
+++ util/graphical_output_inspector.py
@@ -1,9 +1,9 @@
 import argparse
 import dash
 
+import plotly.graph_objects as pgo
 import pandas as pd
 import numpy as np
-import plotly.express as px
 import dash_bootstrap_components as dbc
 
 app = dash.Dash(external_stylesheets=[dbc.themes.COSMO])
@@ -16,35 +16,27 @@ parser.add_argument(
 )
 parser.add_argument(
     "--object_id",
-    default=None,
+    default=[],
     required=False,
-    help="Object ID to be plotted, not specified if object name is used instead.",
+    action="append",
+    help="Object ID to be plotted, option may be repeated to plot multiple objects.",
 )
 parser.add_argument(
     "--object_name",
-    default=None,
+    default=[],
     required=False,
-    help="Object name to be plotted, not specified if object id is used instead.",
+    action="append",
+    help="Object name to be plotted, option may be repeated to plot multiple objects.",
 )
 args = parser.parse_args()
+plot_objects = [{"filter": "name", "value": x} for x in args.object_name] + [
+    {"filter": "id", "value": int(x)} for x in args.object_id
+]
 
-filter_field = None
-filter_value = None
+if not len(plot_objects):
+    raise ValueError("At least one object must be specified as input.")
 
-if args.object_id is not None:
-    filter_field = "id"
-    filter_value = int(args.object_id)
-elif args.object_name is not None:
-    filter_field = "name"
-    filter_value = args.object_name
-else:
-    parser.print_help()
-    exit()
-
-sim_data_iter = pd.read_csv(args.sim_data, iterator=True, chunksize=1000)
-sim_data = pd.concat(
-    [chunk[chunk[filter_field] == filter_value] for chunk in sim_data_iter]
-)
+sim_data = pd.read_csv(args.sim_data)
 
 sim_time_min = sim_data["sim_time"].min()
 sim_time_max = sim_data["sim_time"].max()
@@ -106,6 +98,24 @@ def make_sphere(x, y, z, radius, resolution=100):
     return (X, Y, Z)
 
 
+def format_text_info(row):
+    hover_info = [
+        "name",
+        "sim_time",
+        "x_coord",
+        "y_coord",
+        "z_coord",
+        "altitude",
+        "x_velocity",
+        "y_velocity",
+        "z_velocity",
+    ]
+    text = ""
+    for attr in hover_info:
+        text += f"{attr}: {row[attr]}<br>"
+    return text
+
+
 @dash.callback(
     dash.Output("plot", "figure"),
     dash.Input("switch-control", "value"),
@@ -116,15 +126,25 @@ def draw_plot(switch_control, sim_time_range):
         sim_data["sim_time"] <= sim_time_range[1]
     )
     sim_data_slice = sim_data[time_mask]
-    orbital_line_fig = px.line_3d(
-        sim_data_slice,
-        x="x_coord",
-        y="y_coord",
-        z="z_coord",
-        hover_name="name",
-        hover_data=["x_coord", "y_coord", "z_coord", "sim_time"],
-        title=f"{filter_field}: {filter_value}",
-    )
+
+    orbital_line_fig = pgo.Figure()
+
+    for plot_object in plot_objects:
+        object_data = sim_data_slice[
+            sim_data_slice[plot_object["filter"]] == plot_object["value"]
+        ]
+        text_labels = [format_text_info(row) for _, row in object_data.iterrows()]
+        orbital_line_fig.add_trace(
+            pgo.Scatter3d(
+                x=object_data["x_coord"],
+                y=object_data["y_coord"],
+                z=object_data["z_coord"],
+                text=text_labels,
+                hoverinfo="text",
+                name=object_data["name"].values[0],
+                mode="lines",
+            )
+        )
 
     if "orbit_body" in switch_control:
         orbial_body = make_sphere(0, 0, 0, 6378137.0)
